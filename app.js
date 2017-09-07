a63f0d5d5f0bd52c27cd89db7dd0dd0f83bc55cb
Biagio Santoro <slemeae@gmail.com>
2017-09-07 22:48:01 +0100

    Added comments and reviewed code.

--- app.js
+++ app.js
@@ -1,49 +1,46 @@
-// Load up the discord.js library
+// Load up the discord.js library.
 const Discord = require("discord.js");
 
-// This is your client. Some people call it `bot`, some people call it `self`,
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
+// Defining the Discord Client as "client".
 const client = new Discord.Client();
 
 // Here we load the config.json file that contains our token and our prefix values.
 const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
 
-// Create a new webhook
+// Loading "Missigno" hook for #console in pal-bot discord.
 const hook = new Discord.WebhookClient(config.hook_id, config.hook_token);
 
-// Mainetance on?
+// Here we define mainetance. (0 = off | 1 = on)
 const mainetance = 0
 
-// BOT STARTED UP HERE!!!!!!!!!!!
+// The events under here will run if the bot starts, and logs in, successfully.
 client.on("ready", () => {
-// If you got here it means bot logged in.
+  // Logging in console that bot has logged in and is using discord.js version "xx.x.x".
   console.log("-------------");
   console.log("Logged in!");
-	hook.send(`Logged in!`)
+  hook.send(`Logged in!`)
+  console.log("Using discord.js version 11.2.0");
   console.log("-------------");
-
-  // This event will run if the bot starts, and logs in, successfully.
   console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
 	hook.send(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
   console.log("-------------");
 
+  // The events under here will run if the bot starts, logs in successfully and mainetance is set to "ON".
   if (mainetance == 1) {
-    // client.user.setPresence({ game: { name: 'Under mainetance.', type: 0 } });
+    // Setting bot's game and status.
     client.user.setGame(`Under mainetance.`);
     client.user.setStatus("idle");
-    console.log("Variable mainetance is set to 1. This means the status has been set to idle and presence set to 'Under mainetance'.");
-		hook.send("Variable mainetance is set to 1. This means the status has been set to idle and presence set to 'Under mainetance'.");
+
+    // Logging changes.
+    console.log("Mainetance set to 'ON'. This means the status has been set to idle and presence set to 'Under mainetance'.");
+		hook.send("Mainetance set to 'ON'. This means the status has been set to idle and presence set to 'Under mainetance'.");
     console.log("-------------");
   }else {
-
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  client.user.setGame(`Online on ${client.guilds.size} servers! | +help`);
-  // client.user.setPresence({ game: { name: `Online! | +help `, type: 0 } });
+  // Setting bot's game and status.
+  client.user.setGame(`Online! | +help`);
   client.user.setStatus("online");
+
+  // Logging changes.
   console.log("Bot's status and game set.");
 	hook.send(`Bot's status and game set.`)
   console.log("-------------");
@@ -51,42 +48,43 @@ client.on("ready", () => {
 
 });
 
+// This will run only if the bot has dissconnected, for whatever reason.
 client.on("disconnected", function () {
+  // Logging changes.
 	console.error("Disconnected!");
 	hook.send("Disconnected!");
-	process.exit(1); //exit node.js with an error
+
+  // This should exit node.js with an error.
+	process.exit(1);
 });
 
+// This event triggers only when the bot joins a guild.
 client.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
+  // Logging changes.
   console.log(`New server joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
 	hook.send(`New server joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  // client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
+// This event triggers only when the bot is removed from a guild.
 client.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
+  // Logging changes.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 	hook.send(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  // client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
-
+// This event will run on every single message received, from any channel or DM.
 client.on("message", async(message) => {
-  // This event will run on every single message received, from any channel or DM.
 
+  // If the bot is being pinged, reply with "Hello?".
 	if(message.isMentioned(client.user)) message.channel.send('Hello?');
 
-
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
+  // It's good practice to ignore other bots. This also makes your bot ignore itself and not get into a "botception".
   if(message.author.bot) return;
 
-  // Also good practice to ignore any message that does not start with our prefix,
-  // which is set in the configuration file.
+  // Also good practice to ignore any message that does not start with our prefix, set in the configuration file.
   if(message.content.indexOf(config.prefix) !== 0) return;
 
-
+  // Logging recived commands.
   console.log("Recived " + message.content + " from " + message.author + ". Treating it as a command.");
 	hook.send("Recived " + message.content + " from " + message.author + ". Treating it as a command.");
   console.log("-------------");
@@ -97,19 +95,25 @@ client.on("message", async(message) => {
   // command = say
   // args = ["Is", "this", "the", "real", "life?"]
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+
+  // Make recived command all lower case.
   const command = args.shift().toLowerCase();
 
 
-
-  // Let's go with a few common example commands! Feel free to delete or change those.
-
+  /*
+  Command: ping
+  */
   if(command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip).
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
 
+
+  /*
+  Command: git
+  */
   if(command === "git"){
     message.channel.send({
   embed: {
@@ -141,15 +145,24 @@ client.on("message", async(message) => {
 })
   }
 
+  /*
+  Command: server
+  */
 	if(command === "server"){
 		message.author.send("You can join this bots discord server using https://discord.gg/k6qSHQs")
 	}
 
+  /*
+  Command: invite
+  */
   if(command === "invite") {
       message.react('👌');
 			message.author.send("Bot invite link: https://discordapp.com/oauth2/authorize?&client_id=" + AuthDetails.client_id + "&scope=bot&permissions=470019135");
 }
-// Help command manually made at the moment.
+
+/*
+Command: help
+*/
 if(command === "help"){
   message.react('👌');
   message.channel.send("Check your private messages! :wink:");
@@ -163,6 +176,9 @@ if(command === "help"){
 	message.author.send("You can also join this bots discord server for more help using https://discord.gg/k6qSHQs")
 }
 
+  /*
+  Command: say
+  */
   if(command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use.
     // To get the "message" itself we join the `args` back into a string with spaces:
@@ -173,10 +189,13 @@ if(command === "help"){
     message.channel.send(sayMessage);
   }
 
-if(command === "me"){
-  message.react('👌');
-	console.log("Reacted sending message now...");
-	message.channel.send(
+  /*
+  Command: me
+  */
+  if(command === "me"){
+    message.react('👌');
+	  console.log("Reacted sending message now...");
+	  message.channel.send(
 		{
   embed: {
     title: "User's info",
@@ -220,7 +239,9 @@ console.log("Message sent.");
 console.log("-------------");
 }
 
-
+  /*
+  Command: info
+  */
 	if(command === "info"){
 		message.react('👌');
 		message.author.send({
@@ -272,7 +293,6 @@ console.log("-------------");
 	/*
 	Hook Test
 	*/
-
 	if(command === "hookme") {
 		if(message.author.id !== config.ownerID || config.benID){
 			message.react("👎");
@@ -284,6 +304,9 @@ console.log("-------------");
 		}
 }
 
+  /*
+  Command: purge
+  */
   if(command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
 
@@ -301,11 +324,15 @@ console.log("-------------");
   }
 });
 
-// Checking if you are using a token to log in.
+
+// This checks if bot is using a bot token to log in.
 if(config.token){
+// Log whats happening.
 console.log("-------------");
 console.log("Trying to log in with token...");
+hook.send("Trying to log in with token...");
 client.login(config.token);
 } else {
+// Only will happpen is error. This should only happen if the error is you dont have a bot token.
 console.log("Bot token not found! Remember you cant log in with credentials anymore.");
 }
