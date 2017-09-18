ba8ffd8c46ede40665e3bdff649e0608a1840e98
Biagio Santoro <slemeae@gmail.com>
2017-09-18 15:04:19 +0100

    Fixx?

--- app.js
+++ app.js
@@ -156,7 +156,7 @@ client.on("message", async(message) => {
 			// Calculates ping between sending a message and editing it, giving a nice round-trip latency.
 			// The second ping is an average latency between the bot and the websocket server (one-way, not round-trip).
 			const m = await message.channel.send("Ping?");
-			m.edit("It took ``` " + ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}"ms ``` to ping" + ranQuote);
+			m.edit("It took ``` " + m.createdTimestamp - message.createdTimestamp + "ms ``` to ping" + ranQuote + "\n Also, API" + Math.round(client.ping));
 	}
 
 	/*
