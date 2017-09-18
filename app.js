eea7a62997089f7b5213682da78eb60891c76100
Biagio Santoro <slemeae@gmail.com>
2017-09-18 15:02:15 +0100

    Added some cool shit

--- app.js
+++ app.js
@@ -151,10 +151,12 @@ client.on("message", async(message) => {
 	Command: ping
 	*/
 	if(command === "ping") {
-	    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-	    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip).
-	    const m = await message.channel.send("Ping?");
-	    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+		const pings = ['the moon', 'europe', 'oceania', 'Trump', 'a baguette'];
+		const ranQuote = pings[Math.floor(Math.random() * myArray.length)];
+			// Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+			// The second ping is an average latency between the bot and the websocket server (one-way, not round-trip).
+			const m = await message.channel.send("Ping?");
+			m.edit("It took ``` " + ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}"ms ``` to ping" + ranQuote);
 	}
 
 	/*
