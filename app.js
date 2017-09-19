7fa817f075113368f3ef4b504eaebef2cb49071d
Biagio Santoro <slemeae@gmail.com>
2017-09-19 22:43:21 +0100

    Added config_example.json

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ const games = ['with Dr. Freeman', 'Half Life 3', '+help', 'please send help', '
 setInterval(function(){
 const rangame = games[Math.floor(Math.random() * games.length)];
 client.user.setGame(rangame);
-console.log('Changed game');
 }, 60000)
 
 // The events under here will run if the bot starts, and logs in, successfully.
