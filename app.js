8435b586795ec89fcd9b6cf0089a301f4bd5dfbf
sleme <16744664+sleme@users.noreply.github.com>
2017-10-24 22:23:36 +0100

    Fixed code a bit

--- app.js
+++ app.js
@@ -287,56 +287,37 @@ client.on("message", async(message) => {
 	}
 
 	if (command === "sad"){
-
-			if(!args[0]){
-				return;
-			}
-	message.channel.startTyping()
-	var url = message.author.avatarURL;
-
-	Jimp.read(url).then(function (image) {
-
-	image.resize(1024, 1024, Jimp.RESIZE_BEZIER);
-
-
-		Jimp.loadFont(doRandomSize()).then(function (font) { // load font from .fnt file
-		            // print a message on an image
-		    //image.print(font, 2, 2, args.join(" "), Jimp.ALIGN_FONT_CENTER); // print a message on an image with text wrapped at width
-
-
-				image.greyscale()
-
-
-	    /**image.greyscale()**/image.print(font, 20, 960, args.join(" "), Jimp.ALIGN_FONT_CENTER).getBuffer(Jimp.MIME_JPEG, onBuffer)
-
-
-
-
-			let outputfile = "./output/" + Math.random().toString(36).substr(2, 5) + "sad." + image.getExtension(); // create a random name for the output file
-			image.write(outputfile, function() {
-																// upload file
-																message.channel.sendFile(outputfile).then(function() {
-																// delete file
-																fs.unlink(outputfile);
-																console.log("SUCCESS: " + message.author.username);
-																message.channel.stopTyping()
-																});
-															});
-
-			});
-
-
-
-
-	}).catch(function (err) {
-	    console.error(err);
-	})
-
-	function onBuffer(err, buffer) {
-	    if (err) throw err;
-	    console.log(buffer);
-	}
+		if(!args[0]){
+			return;
 		}
+		message.channel.startTyping()
+		var url = message.author.avatarURL;
+		Jimp.read(url).then(function (image) {
+			image.resize(1024, 1024, Jimp.RESIZE_BEZIER);
+			Jimp.loadFont(doRandomSize()).then(function (font) { // load font from .fnt file
+				// print a message on an image
+				//image.print(font, 2, 2, args.join(" "), Jimp.ALIGN_FONT_CENTER); // print a message on an image with text wrapped at width
+				image.greyscale()
+				/**image.greyscale()**/image.print(font, 20, 960, args.join(" "), Jimp.ALIGN_FONT_CENTER).getBuffer(Jimp.MIME_JPEG, onBuffer)
+				let outputfile = "./output/" + Math.random().toString(36).substr(2, 5) + "sad." + image.getExtension(); // create a random name for the output file
+				image.write(outputfile, function() {
+					// upload file
+					message.channel.sendFile(outputfile).then(function() {
+					// delete file
+					fs.unlink(outputfile);
+					console.log("SUCCESS: " + message.author.username);
+					message.channel.stopTyping()
+				});
+			});
+		});
+	}).catch(function (err) {
+		console.error(err);
+	})
+	function onBuffer(err, buffer) {
+		if (err) throw err;
+		console.log(buffer);
+	}
+}
 
 	/*
 	Command: ping
