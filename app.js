ee7a2501ba1e79db9469707ec4f2fae0840584ca
sleme <16744664+sleme@users.noreply.github.com>
2017-11-11 15:32:27 +0000

    Fixed +clear command

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ try {
 	throw new Error('Node 8.0.0 or higher is required. Please upgrade / update Node.js on your computer / server.')
     }
 } catch (e){
-    console.log(e.stack);
-    console.log("Current Node.js version: " + process.version);
-    console.log("In case you´ve not installed any required module: \nPlease run 'npm install' and ensure it passes with no errors!");
+    console.error();(e.stack);
+    console.error("Current Node.js version: " + process.version);
+    console.error("In case you´ve not installed any required module: \nPlease run 'npm install' and ensure it passes with no errors!");
     process.exit();
 }
 // Log what node version the bot is using and what discord.js verion its using.
@@ -302,23 +302,27 @@ client.on("message", async(message) => {
   	// hook.send("Recived " + message.content + ". Treating it as a command.");
 	console.log("-------------");
 
-/*
   if(command === "clear"){
-    const user = message.mentions.users.first();
-    const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-    if (!amount) return message.reply(':warning: Must specify an amount to delete!');
-    if (!amount && !user) return message.reply(':warning: Must specify a user and amount, or just an amount, of messages to purge!');
-    message.channel.fetchMessages({
-      limit: amount,
-    }).then((messages) => {
-      if (user) {
-        const filterBy = user ? user.id : Client.user.id;
-        messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-      }
-      message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-    });
+    //const mod = message.member
+    if(message.member.hasPermission("MANAGE_MESSAGES")){
+      const user = message.mentions.users.first();
+      const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
+      if (!amount) return message.reply(':warning: Must specify an amount to delete!');
+      if (amount === 1) return message.reply(':warning: Please delete more than one message.');
+      if (!amount && !user) return message.reply(':warning: Must specify a user and amount, or just an amount, of messages to purge!');
+      message.channel.fetchMessages({
+        limit: amount,
+      }).then((messages) => {
+        if (user) {
+          const filterBy = user ? user.id : Client.user.id;
+          messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+        }
+        message.channel.bulkDelete(messages).catch(error =>  message.channel.send(':warning: Oh no error: ' + error));
+      });
+    }else {
+      message.reply(':warning: You must be able to delete messages to use this command!');
+    }
   }
-*/
 
 	/*
 	Command: list-servers
@@ -330,7 +334,7 @@ client.on("message", async(message) => {
 	    }else {
 		    const guilds = client.guilds.array();
 		    for(let guild in guilds) {
-			    hook.send("```" + guilds[guild]["name"] + "```");
+			    message.author.send("```" + guilds[guild]["name"] + "```");
 		    }
 	    }
 	}
